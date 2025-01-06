92d8a3df80c584c48f1e49c91939075e485eef12
Роман <frideriko26_93@mail.ru>
2025-01-06 20:44:10 +0300

    Add frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
