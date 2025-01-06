46a5fe74ea5636816a76840445677f4ea06eb43b
Роман <frideriko26_93@mail.ru>
2025-01-06 21:08:20 +0300

    Add all model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('title',)  # Поля для списка объектов
+    search_fields = ('title',)  # Поиск по названию
+    ordering = ('title',)  # Сортировка по названию
+
+admin.site.register(Location, LocationAdmin)
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-print('fsdf')
+class Location(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lng = models.DecimalField(max_digits=16, decimal_places=14)
+    lat = models.DecimalField(max_digits=16, decimal_places=14)
