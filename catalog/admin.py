b8b5f8700a394a91246d2c33a07dc3d88980249d
Roman <frideriko26_93@mail.ru>
2025-01-07 16:32:34 +0300

    Add model for images

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, Image
 
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('title',)  # Поля для списка объектов
+    list_display = ['title', 'uploaded_at']  # Поля для списка объектов
     search_fields = ('title',)  # Поиск по названию
     ordering = ('title',)  # Сортировка по названию
 
-admin.site.register(Location, LocationAdmin)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['location', 'uploaded_at']
+
+
 
--- catalog/models.py
+++ catalog/models.py
@@ -6,3 +6,15 @@ class Location(models.Model):
     description_long = models.TextField()
     lng = models.DecimalField(max_digits=16, decimal_places=14)
     lat = models.DecimalField(max_digits=16, decimal_places=14)
+    uploaded_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    location = models.ForeignKey('Location', related_name='images', on_delete=models.CASCADE)  # related_name меняем на 'images'
+    image = models.ImageField(upload_to='images/')  # Поле для изображения
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.location.title}"  # Возвращаем название локации в строковом представлении изображения
