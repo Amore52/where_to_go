2c10116ce7ea4f0e55cba07e8430d686523073d5
Roman <frideriko26_93@mail.ru>
2025-01-08 16:54:35 +0300

    update urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', views.main_page),
+    path('places/<int:place_id>/', views.place_details, name='place_details'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -4,36 +4,41 @@ from catalog.models import Location, Image
 import json
 
 def main_page(request):
-    places_data = [
-        {
+    places_data = []
+    locations = Location.objects.all()
+
+    for location in locations:
+        images = Image.objects.filter(location=location)
+        image_urls = [image.image.url for image in images]
+        place_info = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": [37.62, 55.793676]
+                "coordinates": [location.lng, location.lat]  # Долгота и широта
             },
             "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": "/static/places/moscow_legends.json"
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [37.64, 55.753676]
-            },
-            "properties": {
-                "title": "Крыши24.рф",
-                "placeId": "roofs24",
-                "detailsUrl": "/static/places/roofs24.json"
+                "title": location.title,
+                "placeId": location.id,
+                "detailsUrl": f"/places/{location.id}/",
+                "imgs": image_urls
             }
         }
-    ]
-
-    # Преобразуем данные в JSON строку
+        places_data.append(place_info)
     places_geojson = json.dumps(places_data)
-
     return render(request, 'index.html', {
         'places_geojson': places_geojson,
     })
+
+
+def place_details(request, place_id):
+    try:
+        location = Location.objects.get(id=place_id)
+        images = Image.objects.filter(location=location)
+        return JsonResponse({
+            'title': location.title,
+            'description_short': location.description_short,
+            'description_long': location.description_long,
+            'imgs': [image.image.url for image in images],
+        })
+    except Location.DoesNotExist:
+        return JsonResponse({'error': 'Place not found'}, status=404)
