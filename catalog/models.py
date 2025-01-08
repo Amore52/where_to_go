b5f943a83edbb43475aab227519c83ae1dab5322
Roman <frideriko26_93@mail.ru>
2025-01-08 16:17:57 +0300

    Move data from template to view

--- catalog/models.py
+++ catalog/models.py
@@ -4,17 +4,17 @@ class Location(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField()
     description_long = models.TextField()
-    lng = models.DecimalField(max_digits=16, decimal_places=14)
-    lat = models.DecimalField(max_digits=16, decimal_places=14)
+    lng = models.FloatField()
+    lat = models.FloatField()
     uploaded_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
         return self.title
 
 class Image(models.Model):
-    location = models.ForeignKey('Location', related_name='images', on_delete=models.CASCADE)  # related_name меняем на 'images'
-    image = models.ImageField(upload_to='images/')  # Поле для изображения
+    location = models.ForeignKey('Location', related_name='image', on_delete=models.CASCADE, blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Image for {self.location.title}"  # Возвращаем название локации в строковом представлении изображения
+        return f"Image for {self.location.title}"
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,39 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.http import JsonResponse
+from catalog.models import Location, Image
+import json
 
 def main_page(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    places_data = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.62, 55.793676]
+            },
+            "properties": {
+                "title": "«Легенды Москвы",
+                "placeId": "moscow_legends",
+                "detailsUrl": "/static/places/moscow_legends.json"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.64, 55.753676]
+            },
+            "properties": {
+                "title": "Крыши24.рф",
+                "placeId": "roofs24",
+                "detailsUrl": "/static/places/roofs24.json"
+            }
+        }
+    ]
+
+    # Преобразуем данные в JSON строку
+    places_geojson = json.dumps(places_data)
+
+    return render(request, 'index.html', {
+        'places_geojson': places_geojson,
+    })
