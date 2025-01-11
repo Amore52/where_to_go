f535c02a9f6200a504c451203098d43d79d32e49
Roman <frideriko26_93@mail.ru>
2025-01-11 10:27:21 +0300

    update settings.py

--- where_to_go_config/settings.py
+++ where_to_go_config/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
