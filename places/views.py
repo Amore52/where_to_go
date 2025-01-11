6f8a299370e1a68279af7edbb2e7c421e5a5f3d2
Roman <frideriko26_93@mail.ru>
2025-01-11 11:21:11 +0300

    update models.py

--- places/views.py
+++ places/views.py
@@ -40,8 +40,8 @@ def place_details(request, place_id):
         images = Image.objects.filter(location=location)
         return JsonResponse({
             'title': location.title,
-            'short_discription': location.short_description,
-            'long_discription': location.long_discription,
+            'short_description': location.short_description,
+            'long_description': location.long_description,
             'imgs': [image.image.url for image in images],
         })
     except Location.DoesNotExist:
